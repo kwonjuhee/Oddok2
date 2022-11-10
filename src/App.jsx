c018ea0a29e8a59c821224f91d1fcd9c9c14a245
juhee <77ooee@gmail.com>
2022-11-10 17:30:12 +0900

    feat: 전역 로딩 상태 선언

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { ErrorModal } from "@components/@commons";
+import { useRecoilValue } from "recoil";
+import { loadingState } from "@recoil/loading-state";
+import { ErrorModal, Loading } from "@components/@commons";
 import Router from "./routes/Router";
 
 function App() {
+  const isLoading = useRecoilValue(loadingState);
+
   return (
     <BrowserRouter>
+      {isLoading && <Loading />}
       <ErrorModal />
       <Router />
     </BrowserRouter>
