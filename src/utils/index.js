d7bceb7ecd5952ae784c96cd7b83a7294713bf98
juhee <77ooee@gmail.com>
2022-11-15 00:54:30 +0900

    style: 불필요한 유틸 함수 제거

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export { default as getColor } from "./getColor";
 export { default as getDday } from "./getDday";
 export { default as dateFormatting } from "./dateFormatting";
 export { default as dateParsing } from "./dateParsing";
