056cc8dd2e8c915a2c3f3e0f8c3cc7e2bc972c46
juhee <77ooee@gmail.com>
2022-11-21 15:01:10 +0900

    refactor: 불필요한 컴포넌트 제거

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useRef } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { PageLayout } from "@components/@layouts";
-import { InputForm, SearchBrowse, SearchResult } from "@components/search";
+import { SearchBrowse, SearchResult } from "@components/search";
+import { Input } from "@components/@commons";
+import { useSearchHistory } from "@hooks";
+import styles from "./Search.module.css";
 
 function Search() {
+  const keywordRef = useRef();
+  const navigate = useNavigate();
+  const { addHistory } = useSearchHistory();
+
+  const handleSearchKeyword = (e) => {
+    e.preventDefault();
+    if (keywordRef.current.value === "") return;
+
+    navigate({ pathname: "/search/studyroom", search: `?name=${keywordRef.current.value}` });
+    addHistory(keywordRef.current.value);
+    keywordRef.current.blur();
+  };
+
   return (
     <PageLayout>
-      <InputForm />
+      <form className={styles.input_wrapper} onSubmit={handleSearchKeyword}>
+        <Input ref={keywordRef} />
+      </form>
       <Routes>
         <Route path="/" element={<SearchBrowse />} />
         <Route path="/studyroom" element={<SearchResult />} />
