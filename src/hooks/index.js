2b666eaad3f96f9a82eb6703568d79eaafe6eaf6
juhee <77ooee@gmail.com>
2022-11-23 20:08:29 +0900

    remove: 사용하지 않는 훅 삭제

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,3 @@
-export { default as useAsync } from "./useAsync";
 export { default as useInput } from "./useInput";
 export { default as useModal } from "./useModal";
 export { default as useOutSideClick } from "./useOutSideClick";
